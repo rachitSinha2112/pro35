c3369f88ff3439fbbc1550f4bfe126d2e3efb7fa
rachitSinha2112 <71123839+rachitSinha2112@users.noreply.github.com>
2021-06-09 23:15:25 +0530

    Update food.js

--- js/food.js
+++ js/food.js
@@ -6,7 +6,8 @@ constructor(){
 }
 getFoodStock(){
      var foodStockRef=database.ref("dog/food")
-     foodStockRef.on("value",function(data){
+     foodStockRef.on("value",(data)=>{
+        console.log(data.val())
         this.foodStock=data.val()
      })
     }
@@ -22,8 +23,8 @@ display(){
  var x=80,y=100;
   imageMode(CENTER);
   
-console.log(this.foodStock)
-console.log(this.lastFed)
+
+//console.log(this.lastFed)
 
  image(this.image,520,70,70,70)
 if(this.foodStock!==0){
@@ -32,7 +33,7 @@ if(this.foodStock!==0){
            x=50;
            y=y+50;
          }
-         image(this.image,x,y,50,50);
+         image(this.image,x,y,60,60);
          x=x+30
      }
  }
